bed672c7cdbc0d450ab06746fb86dfca975224e8
lgk03 <lucaakollmer@gmail.com>
2025-04-19 00:49:49 +0200

    changes metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,14 +1,16 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Data Analysis Student Club at TUM',
+  author: 'Luca Kollmer',
+  headerTitle: 'TUMDA',
+  description:
+    'TUMDA is the Data Analysis Student Club at TUM. We are a group of students interested in data science, machine learning, and artificial intelligence.',
+
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/tumda-logo-v1.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
