1b5a9d32b2eca2fc57198927f1a26b5e088e45e0
Luca K <97964020+lgk03@users.noreply.github.com>
2025-07-12 11:11:59 +0200

    Revert "Adjust/about us"

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,28 +2,18 @@ import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
-import { Timeline } from "@/components/Timeline";
-import { timelineData } from "@/data/timelineData";
-
 import { genPageMetadata } from 'app/seo'
-
-
 
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const why = allAuthors.find((p) => p.slug === 'whyjoin') as Authors
   const mainContent = coreContent(author)
 
   return (
     <>
-      <AuthorLayout content={mainContent} title={"About"} >
+      <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-      <Timeline data={timelineData} />
-          <AuthorLayout content={mainContent} title={"Why Join?"}>
-        <MDXLayoutRenderer code={why.body.code} />
       </AuthorLayout>
     </>
   )
