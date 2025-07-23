713755ac4c0c47e092ab4cf09fa03ca568cd9a10
lgk03 <lucaakollmer@gmail.com>
2025-07-23 14:57:49 +0200

    rm everything that is cursed

--- data/eventsData.ts
+++ data/eventsData.ts
@@ -7,13 +7,13 @@ interface Event {
 }
 
 const eventsData: Event[] = [
-  {
-    title: 'Visit to Leading Tech Consultancy',
-    description: `A group of our students had the unique opportunity to visit one of the world's top tech consultancies. We explored how data analytics and process mining are transforming the consulting industry, participated in hands-on workshops, and networked with experienced consultants. The event inspired many of us to pursue ambitious projects and careers in data-driven innovation!`,
-    date: '2025-06-20',
-    location: 'New York, USA',
-    imgSrc: '/static/images/consultancy-visit.jpg',
-  },
+  // {
+  //   title: 'Visit to Leading Tech Consultancy',
+  //   description: `A group of our students had the unique opportunity to visit one of the world's top tech consultancies. We explored how data analytics and process mining are transforming the consulting industry, participated in hands-on workshops, and networked with experienced consultants. The event inspired many of us to pursue ambitious projects and careers in data-driven innovation!`,
+  //   date: '2025-06-20',
+  //   location: 'New York, USA',
+  //   imgSrc: '/static/images/consultancy-visit.jpg',
+  // },
 ]
 
 export default eventsData
