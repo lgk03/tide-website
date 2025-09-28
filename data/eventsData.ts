7a8bb0492df58532e8df159545dae09f602e9dba
Arthur920 <arthur.gersbacher@gmail.com>
2025-09-28 21:13:35 +0200

    added db project, adjusted db event name

--- data/eventsData.ts
+++ data/eventsData.ts
@@ -8,7 +8,7 @@ interface Event {
 
 const eventsData: Event[] = [
   {
-    title: 'DB Delay Desktop',
+    title: 'DB Delay Desktop Kickoff',
     description: `We’re analyzing Deutsche Bahn data to explore the current state of Germany’s public transportation infrastructure. Join our planning meeting to help shape the project. Interested? Get in touch at tide.tum@gmail.com.`,
     date: '2025-09-28',
     location: 'virtual',
