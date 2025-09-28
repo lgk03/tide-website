56347d0624251da3fad143332fa5051092e07b99
Arthur920 <arthur.gersbacher@gmail.com>
2025-09-28 10:41:52 +0200

    adjusted text

--- data/eventsData.ts
+++ data/eventsData.ts
@@ -9,7 +9,7 @@ interface Event {
 const eventsData: Event[] = [
   {
     title: 'DB Delay Desktop',
-    description: `tbd`,
+    description: `We’re analyzing Deutsche Bahn data to explore the current state of Germany’s public transportation infrastructure. Join our planning meeting to help shape the project. Interested? Get in touch at tide.tum@gmail.com.`,
     date: '2025-09-28',
     location: 'virtual',
     imgSrc: '/static/images/deutsche-bahn-verspaetungen-anzeigetafel.jpg',
